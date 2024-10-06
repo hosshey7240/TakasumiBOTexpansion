939ab2be2a6154e67a5164f1072202920f84580f
hosshey_7240 <129696997+hosshey7240@users.noreply.github.com>
2024-10-06 20:31:18 +0900

    Update TakasumiBOTexpansionbata.py(不足点を追加)

--- TakasumiBOTexpansionbata.py
+++ TakasumiBOTexpansionbata.py
@@ -15,6 +15,14 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+@client.event
+async def on_message(message):
+    if message.author.id == 981314695543783484 and len(message.embeds) == 1 and "コイン手に入れました" in message.embeds[0].author.name:
+        embed = discord.Embed(title="TakasumiBOT work通知", description="workを受信しました。\n20分後に通知します。", color=discord.Color.brand_green())
+        await message.reply(embed=embed)
+        await asyncio.sleep(1200)
+        embed2 = discord.Embed(title="TakasumiBOT work通知", description="workの時間です\n</work:1132868147519692871>でお金を得ましょう", color=discord.Color.brand_green())
+        await message.channel.send(embed=embed2)
 
 @client.event
 async def on_ready():
